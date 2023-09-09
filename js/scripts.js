f4c0dc506c2643457df13accc13bfcca30c0939c
Vladislav <fletspleyhik@gmail.com>
2023-09-09 14:14:45 +0300

    (Vlad Musikhin) Finished all task!

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const clickableBlock = document.getElementById('clickableBlock');
 
-  clickableBlock.addEventListener('click', function() {
+  clickableBlock.addEventListener('click', function () {
     window.location.href = 'item.html';
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const backMainPage = document.getElementById('backMainPage');
 
-document.addEventListener('DOMContentLoaded', function() {
+  backMainPage.addEventListener('click', function () {
+    window.location.href = 'index.html';
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
   const openMenuDiv = document.getElementById('openSideBar');
+  const closeButton = document.getElementById('closeButton');
   const sideMenu = document.getElementById('sideBar');
   const mainContent = document.getElementById('mainContent');
 
-  openMenuDiv.addEventListener('click', function() {
+  openMenuDiv.addEventListener('click', function () {
     if (sideMenu.classList.contains('open')) {
       sideMenu.classList.replace('open', 'closed');
       mainContent.classList.replace('cardsNone', 'cards');
     } else {
       sideMenu.classList.replace('closed', 'open');
       mainContent.classList.replace('cards', 'cardsNone');
+    }
+  });
+  closeButton.addEventListener('click', function () {
+    if (sideMenu.classList.contains('closed')) {
+      sideMenu.classList.replace('closed', 'open');
+      mainContent.classList.replace('cards', 'cardsNone');
+    } else {
+      sideMenu.classList.replace('open', 'closed');
+      mainContent.classList.replace('cardsNone', 'cards');
     }
   });
 });
