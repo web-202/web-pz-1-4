13f42e55f86b3bde23fc4c33e2ed86e189bcd099
Игорь Пустовойт <pustovoit.igor2018@gmail.com>
2023-10-05 11:49:27 +0300

    0

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const body = document.body;
+
+    sidebar.classList.toggle('active');
+    body.classList.toggle('active-sidebar');
+}
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const body = document.body;
+
+    sidebar.classList.toggle('active');
+    body.classList.toggle('active-sidebar');
+}
