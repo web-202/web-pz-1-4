a94e4dd6f4f5f46cdc161010af17113f997908dd
syurii10 <syurij37@gamil.com>
2023-10-08 11:26:09 +0300

    1.0.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openCloseNavMenu() {
+  const navMenuDiv = document.getElementById('nav-menu')
+  navMenuDiv.style.display = navMenuDiv.style.display === 'none' ? 'block' : 'none'
+
+  const mainTag = document.getElementById('main')
+  mainTag.style.display = navMenuDiv.style.display === 'none' ? 'block' : 'none'
+
+  const headerTag = document.getElementById('header')
+  headerTag.style.display = navMenuDiv.style.display === 'none' ? 'block' : 'none'
+}
